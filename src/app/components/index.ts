8bf3042dc50c50564f6f6bdefab8e803fbda4483
Zachary Wiseman <zack21wiseman@gmail.com>
2018-10-07 20:23:18 -0400

    finishing login component and adding the demo component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,10 +1,13 @@
 import { DescriptionComponent } from './description/description.component';
 import { LoginComponent } from 'components/login/login.component';
+import { DemoComponent } from 'components/demo/demo.component';
 
 export * from './description/description.component';
 export * from './login/login.component';
+export * from './demo/demo.component';
 
 export const Components = [
     DescriptionComponent,
+    DemoComponent,
     LoginComponent
 ];
